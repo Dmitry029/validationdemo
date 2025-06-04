0cadcc0805f632c37ba622886f7c503ab89e79ad
Dmitry029 <kdp.029029@cmail.com>
2025-06-04 17:59:29 +0300

    add custom message for errors

--- src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -11,9 +11,10 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be >= 0")
     @Max(value = 10, message = "must be <= 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
