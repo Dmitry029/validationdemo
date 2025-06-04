c7f8dd0c72d2d5fede4252701477307d524a5ef5
Dmitry029 <kdp.029029@cmail.com>
2025-06-04 17:23:57 +0300

    add regexp for validation

--- src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,21 +1,20 @@
 package com.luv2code.springdemo.mvc;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class Customer {
     private String firstName;
 
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String lastName;
+
     @Min(value = 0, message = "must be >= 0")
     @Max(value = 10, message = "must be <= 10")
     private int freePasses;
 
-
-    @NotNull(message = "is required")
-    @Size(min = 1, message = "is required")
-    private String lastName;
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 }
