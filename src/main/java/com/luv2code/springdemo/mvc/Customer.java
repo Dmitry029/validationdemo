52636c5d865e44384e19287bd82855d821b96f50
Dmitry029 <kdp.029029@cmail.com>
2025-06-04 16:28:14 +0300

    add validation a number range

--- src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -8,7 +10,12 @@ import lombok.Data;
 public class Customer {
     private String firstName;
 
+    @Min(value = 0, message = "must be >= 0")
+    @Max(value = 10, message = "must be <= 10")
+    private int freePasses;
+
+
     @NotNull(message = "is required")
-    @Size(min=1, message = "is required")
+    @Size(min = 1, message = "is required")
     private String lastName;
 }
